ebe503b2ba036009968d38e0bc217144db11edc1
Márcio Filho <51715526+marcyomacedo@users.noreply.github.com>
2021-07-21 10:29:01 -0300

    Initial commit

---
+++ src/main/java/behoh/desafio/DesafioJavaWebApplication.java
@@ -0,0 +1,13 @@
+package behoh.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJavaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/behoh/desafio/DesafioJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package behoh.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
